c780848f41c5c4ce37adc046faab092d8f5a2036
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-02-19 16:50:40 -0500

    DEV: completed Assignment 2. Added Movie details page, and collection view cells for superheroes, and completed the first bonus of connecting the cells from the container to the movies detail page.

--- flixter/MoviesViewController.swift
+++ flixter/MoviesViewController.swift
@@ -19,9 +19,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
-        
-        print("HELLO")
-        
+     
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -36,11 +34,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             
             self.tableView.reloadData()
             
-            print (dataDictionary)
+          
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
-            print(self.movies)
            }
         }
         task.resume()
@@ -66,14 +63,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    
+    }
     /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
     */
 
 }
